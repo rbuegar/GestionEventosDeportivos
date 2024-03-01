d106003133629955bf29f618f68f1d3ef4ba6b72
rbuegar <rbuegar927@g.educaand.es>
2024-03-01 11:40:50 +0100

    adicion de codigo 3 - Roberto

---
+++ src/classes/Ganador.java
@@ -0,0 +1,9 @@
+package classes;
+
+import java.util.ArrayList;
+
+public interface Ganador {
+    // Método que devuelve un objeto de tipo Participante que representa al ganador del evento
+    ArrayList<Participante> obtenerGanador();
+}
+
